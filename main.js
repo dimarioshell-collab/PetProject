3a935bbcbc3bff7fffd0e63d58f960d6e80fb2f4
dimario <dimarioshell@gmail.com>
2026-07-27 18:07:59 +0300

    подготовка сцены

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ window.innerWidth / window.innerHeight, 0.1, 1000)
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -15,10 +16,17 @@ scene.add(cube);
 
 camera.position.z = 5;
 
-function animate() {
+function animate( time ) {
+
+    cube.rotation.x = time / 2000;
+    cube.rotation.y = time / 1000;
+
     renderer.render(scene, camera);
 }
-renderer.setAnimationLoop( animate );
+
+
+
+
 
 
 
