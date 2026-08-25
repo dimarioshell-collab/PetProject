39d29d13c95e9cbaffd99088e79397ca9b441db8
dimario <dimarioshell@gmail.com>
2026-08-25 16:23:12 +0300

    создание обьекта на плоскости

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ import * as THREE from "three";
 const geometry = new THREE.PlaneGeometry(50, 50); // ширина, высота
 
 // Создаём материал
-const material = new THREE.MeshStandardMaterial({ 
+const material = new THREE.MeshStandardMaterial({
   color: 0x3a5f3a,
-  side: THREE.DoubleSide 
+  side: THREE.DoubleSide,
 });
 
 // Создаём меш (объединяем геометрию и материал)
@@ -33,8 +33,14 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000,
 );
-camera.position.z = -2; // adjust based on your model's scale
+camera.position.z = 5; // adjust based on your model's scale
 camera.position.y = 2; // adjust based on your model's scale
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const geometrys = new THREE.BoxGeometry(1, 1, 1); // ширина, высота, глубина
+const materials = new THREE.MeshStandardMaterial({ color: 0x00ff88 });
+const cube = new THREE.Mesh(geometrys, materials);
+scene.add(cube);
+
 renderer.render(scene, camera);
